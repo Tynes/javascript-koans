647b2e74d31adbf1f93224086c37cb4eb30ba6c1
tynes <mtynewa1@binghamton.edu>
2016-01-23 21:41:17 -0500

    Found the smallest digit divisible by each number 1-20
    The answer that I found is 232792560

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -113,26 +113,88 @@ describe("About Applying What We Have Learnt", function() {
 
   /*********************************************************************************/
   /* UNCOMMENT FOR ADVANCED */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
   
+  it("should find the largest prime factor of a composite number", function () {
+    expect(undefined).toBe(1);
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
+    expect(undefined).toBe(1);
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
-    
+      //returns 232792560
+      //skips over checking 1-10 because anything divisible by those digits
+      //must also be divisible by those in 10-20
+      var divisibilityChecker = function() {
+      var target = 11;
+      var isDivisible = false;
+      while(!isDivisible) {
+        if(!(target % 20 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 19 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 18 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 17 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 16 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 15 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 14 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 13 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 12 === 0)) {
+          target += 11;
+          continue;
+        }
+        if(!(target % 11 === 0)) {
+          target += 11;
+          continue;
+        }
+        isDivisible = true;
+      }
+      return target;
+    }
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
+    var sumOfSquares = function(num1, num2) {
+      return (num1 * num1) + (num2 * num2);
+    };
+    var squareOfSums = function(num1, num2) {
+      var sum = num1 + num2;
+      return sum * sum;
+    };
+    var difference = function(num1, num2) {
+      return num1 - num2;
+    };
+    var squares = sumOfSquares(2, 2);
+    var sums = squareOfSums(2, 2);
+
+    expect(difference(sums, squares)).toBe(8);
   });
 
   it("should find the 10001st prime", function () {
-
+    expect(undefined).toBe(1);
   });
-  */
+  
 });
